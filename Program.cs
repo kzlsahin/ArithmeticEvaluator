d500b05b26894800ece1fcb37320c71a57dd2845
Mustafa Şentürk <46689277+kzlsahin@users.noreply.github.com>
2022-07-23 21:12:06 +0300

    New test set added

--- Program.cs
+++ Program.cs
@@ -26,10 +26,12 @@ namespace ArithmeticEvaluator
             PrintLine = Console.WriteLine;
             Print = Console.Write;
 
-            string[] testExpressions_1 = { "24 + 3", "0 + 0", "0 + 1", "+ 2", "2 + ", "5 - 2", "2 - 8", "5 + 11 - 3", "2*5 + 3 - 9", "-10 + 5", "5 - 6 + 10 / 2", "4 - 2 - 5", "4 - 4 - 3" };
+           string[] testExpressions_1 = { "24 + 3", "0 + 0", "0 + 1", "+ 2", "2 + ", "5 - 2", "2 - 8", "5 + 11 - 3", "2*5 + 3 - 9", "-10 + 5", "5 - 6 + 10 / 2", "4 - 2 - 5", "4 - 4 - 3" };
             double[] testResults_1 = { 24 + 3, 0 + 0, 0 + 1, 2, 2, 5 - 2, 2 - 8, 5 + 11 - 3, 2 * 5 + 3 - 9, -10 + 5, 5 - 6 + 5, 4 - 2 - 5, 4 - 4 - 3 };
             string[] testExpressions_2 = { "2 * 3", "1 * 2", "* 2", "4 / 2", "5 * 3 * 4", "0 * 2 ", "0*2", "1*4", "5*5", "5^2", "5^0", "(4 + 2) / 2", "(4 + 2)/2", "4 ^ ( -2)" };
             double[] testResults_2 = { 6, 2, 2, 2, 60, 0, 0, 4, 25, 25, 1, 3, 3, 2 };
+            string[] testExpressions_3 = {"(5)", "1 - 2 * (5 + 12 / 4)","(2 + 2 * (3 - 1))", "16^(1/2) * 2", "((5 + 6/2) + 2 * 3) + 9 - 22 / 2 * 3", "(3+2)*(2)" };
+            double[] testResults_3 = {5, (1 - 2 * (5 + 12 / 4)), 6, 8, (((5 + 6 / 2) + 2 * 3) + 9 - 22 / 2 * 3), 10 };
 
             int countFailed = 0;
 
@@ -40,6 +42,10 @@ namespace ArithmeticEvaluator
             for (int i = 0; i < testExpressions_2.Length; i++)
             {
                 if (!TestAritma(testExpressions_2[i], testResults_2[i])) countFailed++;
+            }
+            for (int i = 0; i < testExpressions_3.Length; i++)
+            {
+                if (!TestAritma(testExpressions_3[i], testResults_3[i])) countFailed++;
             }
 
             PrintLine($"number of failed tests : {countFailed}");
